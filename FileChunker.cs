d6c18d0d480ed6566c6235ac8eefa229a74ba3f7
gary-wilson <g.wilson@parcel2go.com>
2024-05-10 16:00:28 +0100

    basic file

---
+++ FileChunker.cs
@@ -0,0 +1,64 @@
+﻿using System.IO.MemoryMappedFiles;
+
+namespace billions
+{
+    public static class FileChunker
+    {
+        public unsafe static void Start(string file, int threads)
+        {
+            using var fs = File.OpenRead(file);
+            var mmf = MemoryMappedFile.CreateFromFile(fs, null, 0, MemoryMappedFileAccess.Read, HandleInheritability.None, false);
+
+            long batchSize = fs.Length / threads;
+            long totalLines = 0;
+            long totalBatches = (fs.Length / batchSize); // calculate total batches
+            Console.WriteLine($"Total batches: {totalBatches}");
+            Thread[] threadArray = new Thread[threads];
+            for (int t = 0; t < threads; t++)
+            {
+                threadArray[t] = new Thread(() =>
+                {
+                    for (long batch = t; batch < totalBatches; batch += threads)
+                    {
+                        long offset = batch * batchSize;
+                        long size = Math.Min(batchSize, fs.Length - offset);
+                        int lines = 0;
+
+                        using MemoryMappedViewAccessor accessor = mmf.CreateViewAccessor(offset, size, MemoryMappedFileAccess.Read);
+                        byte* p = null;
+                        accessor.SafeMemoryMappedViewHandle.AcquirePointer(ref p);
+
+                        try
+                        {
+                            for (int i = 0; i < size; i++)
+                            {
+                                if (p[i] == 10)
+                                {
+                                    lines++;
+                                }
+                            }
+                        }
+                        finally
+                        {
+                            accessor.SafeMemoryMappedViewHandle.ReleasePointer();
+                        }
+
+                        Interlocked.Add(ref totalLines, lines);
+                    }
+                })
+                {
+                    Priority = ThreadPriority.AboveNormal,
+                    //IsBackground = false,
+                };
+                threadArray[t].Start();
+            }
+
+            for (int t = 0; t < threads; t++)
+            {
+                threadArray[t].Join();
+            }
+
+            Console.WriteLine($"Lines: {totalLines}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+
+namespace billions;
+
+internal sealed class Program
+{
+    static void Main(string[] args)
+    {
+
+        Stopwatch sw = Stopwatch.StartNew();
+        Console.WriteLine("Started");
+
+        FileChunker.Start("C:\\temp\\measurements-1000000000.txt", 20);
+        sw.Stop();
+        Console.WriteLine($"Finished in {sw.ElapsedMilliseconds}ms");
+    }
+}
